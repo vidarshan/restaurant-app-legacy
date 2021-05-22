515cd21f32dd7789fae1b74316a60e0f2050c484
vidarshanadithya98 <vidarshanadithya3@gmail.com>
2021-05-22 20:19:09 +0530

    Code refactor

--- frontend/src/screens/OrderScreen.js
+++ frontend/src/screens/OrderScreen.js
@@ -1,10 +1,13 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { addToOrder, removeFromOrder } from '../actions/orderActions';
 import { Link } from 'react-router-dom';
 import Emoji from '../components/Emoji';
+import { map } from 'lodash';
 
 const OrderScreen = ({ match, location, history }) => {
+  let addOnNames = useRef([{}]);
+
   var randomColor = require('randomcolor');
   var color = randomColor({
     count: 8,
@@ -15,7 +18,6 @@ const OrderScreen = ({ match, location, history }) => {
 
   const qty = location.search ? Number(location.search.split('=')[1]) : 1;
   const size = '';
-  console.log(location);
 
   const dispatch = useDispatch();
 
@@ -23,8 +25,6 @@ const OrderScreen = ({ match, location, history }) => {
   const { orderItems } = order;
 
   const removeFromOrderHandler = (id) => {
-    console.log(id);
-    console.log(orderItems);
     dispatch(removeFromOrder(id));
   };
 
@@ -36,79 +36,105 @@ const OrderScreen = ({ match, location, history }) => {
 
   return (
     <section className='section bd-container-order' id='menu'>
-      <div class='box box1'>
+      <div class='order-list'>
         <div className='order-item-grid'>
           <div className='order-item-category'>
             Pizza <Emoji symbol='🍕' />
           </div>
-          <div className='order-item'>
-            <div className='order-item-image'>
-              <img width='70' height='70' src='./images/pizza4.jpg' alt=' ' />
-            </div>
-            <div className='title-description-addons-container'>
-              <div className='order-item-title'>Philly Cheese Steak</div>
-              <div className='order-item-description'>
-                best Pizza that tastes like no other
+          {map(orderItems, (o) => {
+            return (
+              <div className='order-item'>
+                <div className='order-item-image'>
+                  <img width='70' height='70' src={o.image} alt=' ' />
+                </div>
+                <div className='title-description-addons-container'>
+                  <div className='order-item-title'>{o.name}</div>
+                  <div className='order-item-description'>
+                    best Pizza that tastes like no other
+                  </div>
+                  <div className='order-item-addOns'>
+                    <div className='addOn'>Pineapple</div>
+                    <div className='addOn'>Jalapeno</div>
+                    <div className='addOn'>Olives</div>
+                    <div className='addOn'>Tomato</div>
+                    <div className='addOn'>Cheese</div>
+                    <div className='addOn'>Mayo</div>
+                  </div>
+                </div>
+                <div className='order-item-unit-quantity'>
+                  <div className='order-quantity'>{o.qty}</div>
+                </div>
+                <div className='total-price-unit-quantity-container'>
+                  <div className='order-total-price'>{o.qty * o.price}</div>
+                </div>
+                <div className='order-item-remove'>
+                  <div className='order-remove-btn-container'>
+                    <box-icon
+                      size='20px'
+                      name='trash'
+                      color='#ffffff'></box-icon>
+                  </div>
+                </div>
               </div>
-              <div className='order-item-addOns'>
-                <div className='addOn'>Jalapeno</div>
-                <div className='addOn'>Pineapple</div>
-                <div className='addOn'>Cheese</div>
+            );
+          })}
+        </div>
+      </div>
+      <div class='order-summary'>
+        <div className='order-summary-container'>
+          <div className='order-summary-grid'>
+            <div className='order-summary-left'>
+              <div className='order-summary-total-title'>Gross Total</div>
+            </div>
+            <div className='order-summary-right'>
+              <div className='order-summary-total-price'>300.00</div>
+            </div>
+          </div>
+          <div className='order-summary-grid'>
+            <div className='order-summary-left'>
+              <div className='order-summary-total-title'>Items</div>
+            </div>
+            <div className='order-summary-right'>
+              <div className='order-summary-total-price'>3</div>
+            </div>
+          </div>
+          <div className='order-summary-grid'>
+            <div className='order-summary-left'>
+              <div className='order-summary-total-title'>
+                Service Charge (5%)
               </div>
             </div>
-            <div className='order-item-unit-quantity'>
-              <div className='order-quantity'>2 nos</div>
+            <div className='order-summary-right'>
+              <div className='order-summary-total-price'>$3.00</div>
             </div>
-            <div className='total-price-unit-quantity-container'>
-              <div className='order-total-price'>$100.99</div>
+          </div>
+          <div className='order-summary-grid'>
+            <div className='order-summary-left'>
+              <div className='order-summary-total-title'>Discount</div>
             </div>
-            <div className='order-item-remove'>
-              <div className='order-remove-btn-container'>
-                <box-icon size='20px' name='trash' color='#ffffff'></box-icon>
+            <div className='order-summary-right'>
+              <div className='order-summary-total-price'>$7.00</div>
+            </div>
+          </div>
+          <div className='order-summary-grid'>
+            <div className='order-summary-left'>
+              <div className='order-summary-total-title accent-color'>
+                Net Total
+              </div>
+            </div>
+            <div className='order-summary-right'>
+              <div className='order-summary-total-price accent-color'>
+                $30.00
               </div>
             </div>
           </div>
-        </div>
-      </div>
-      <div class='box box2'>Box 2</div>
-      {/* <div className='flex-order-container'>
-        {orderItems.map((o) => {
-          return (
-            <div className='order-item-container'>
-              <div className='order-details'>
-                <div className='order-item-title'>{o.name}</div>
-                <div className='order-item-unit-price-quanity'>
-                  {o.price} x {o.qty}
-                </div>
-                <div className='order-item-total-price'>{o.price * o.qty}</div>
-              </div>
-              <div className='order-item-image'>
-                <img width='100' height='100' src={o.image} alt=' ' />
-              </div>
-              <div className='order-item-remove-button-container'>
-                <Link
-                  className='remove-from-order-button'
-                  onClick={() => removeFromOrderHandler(o.meal)}>
-                  <box-icon size='20px' name='trash' color='#ffffff'></box-icon>
-                </Link>
-              </div>
-            </div>
-          );
-        })}
-      </div>
-      <div className='order-summary-container'>
-        <div className='order-summary-left'>
-          <div className='order-summary-total'>Total 300.00</div>
-          <div className='order-summary-items'>3 Items</div>
-        </div>
 
-        <div className='order-summary-right'>
           <div className='proceed-to-checkout-button'>
             Proceed to Checkout
             <box-icon name='right-arrow-alt' color='#ffffff'></box-icon>
           </div>
         </div>
-      </div> */}
+      </div>
     </section>
   );
 };
