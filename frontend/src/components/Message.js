63b5312f970a29aabf5ac1d897bc666a462d12be
vidarshanadithya98 <vidarshanadithya3@gmail.com>
2021-05-30 23:17:27 +0530

    Finalizing login and signup

--- frontend/src/components/Message.js
+++ frontend/src/components/Message.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Alert } from 'react-bootstrap';
 
 const Message = ({ id, variant, message, size }) => {
   return (
-    <Alert className={`custom-alert-${variant}-${size}`} key={id}>
-      {message}
-    </Alert>
+    <>
+      <div className={`custom-alert-${variant}-${size}`} key={id}>
+        {message}
+      </div>
+    </>
   );
 };
 
